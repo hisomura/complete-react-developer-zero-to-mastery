9c5bcd022adae7caa43e66f6091816b24a338878
hisomura <hisomura.noreply@gmail.com>
2020-08-18 06:53:51 +0900

    Finish on lecture 45.

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ class App extends Component<{}, State> {
     const filteredMonsters = monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox placeholder='search monsters' handleChange={this.onSearchChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
